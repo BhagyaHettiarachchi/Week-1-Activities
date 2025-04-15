ffab2e5642cddea30c97b3416c65d005dcca1ab7
BhagyaHettiarachchi <bhettiarachchi95@gmail.com>
2025-04-15 20:11:22 +1200

    week1_HWActivity1

---
+++ Activity2_PositiveIntegers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+positive_integers = np.array([1,2,3,4,5,6,7,8,9,10])
+
+# Print the array
+print("First 10 Positive Integers:", positive_integers)
+
+# Print the shap of the array
+print("Shape of Array:", positive_integers.shape)
+
+# Print the data type of the array
+print("Data Type of Array:",positive_integers.dtype)
+
+# Multiply each element by 2 and print the result
+result = positive_integers * 2
+print("Array after multiply each element by 2:", result)
